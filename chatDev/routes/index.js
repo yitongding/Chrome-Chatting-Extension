9e43c001409e68aba6d0aa88da03dc0db8cf71c9
Yitong Ding <404776130@qq.com>
2016-10-10 22:22:15 -0500

    add url support

--- chatDev/routes/index.js
+++ chatDev/routes/index.js
@@ -1,10 +1,12 @@
 module.exports = function(io) {
   var app = require('express');
   var router = app.Router();
+  var url = null;
 
   router.get('/', function(req, res, next) {
+    url = req.param('url');
     res.render('index', {
-      title: 'Express'
+      title: 'Chat'
     });
   });
 
@@ -12,27 +14,15 @@ module.exports = function(io) {
     // connection test
     socket.emit('socketToMe', "connection established");
 
-    // tmp room solution, change to URL later
-    /*************************/
     socket.username = "tmp";
-    //var roomName = "tmp_room";
-    //socket.currentRoom = roomName;
-    //socket.join(roomName);
-    /*^^^^^^^^^^^^^^^^^^^^^^^*/
-
-    // request client current url on connection to assign the room
-    // socket.emit('url req', "url request");
-
-    socket.on('url res', function(url) {
-      //#### add url decode in the future
-      var room = url;
-      socket.room = room;
-      socket.join(room);
-      console.log("new user: <" + socket.username + ">join room <" + url + ">");
-      io.to(room).emit('new member', {
-        username: socket.username
-      });
+    // add user to room based on url
+    var room = url;
+    socket.room = room;
+    socket.join(room);
+    io.to(room).emit('new member', {
+      username: socket.username
     });
+    console.log("new user: <" + socket.username + ">join room <" + url + ">");
 
 
     // get new username
