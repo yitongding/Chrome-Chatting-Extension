8b417b72cb4e6ede65b0f37f5174331e6ac07a42
Yitong Ding <404776130@qq.com>
2016-11-19 16:33:29 -0600

    chatNg v1.10

--- chatNg/public/javascripts/controllers.js
+++ chatNg/public/javascripts/controllers.js
@@ -1,5 +1,5 @@
 // Controller for the coming message
-function ChatMsgCtrl($scope, $routeParams, socket, lastTenMsg, topFiveMsg, historyMsg) {
+function ChatMsgCtrl($scope, $rootScope, $routeParams, socket, lastTenMsg, topFiveMsg, historyMsg) {
 	$scope.room = $routeParams.room;
 	$scope.blockList = [];
 	$scope.alerts = [];
@@ -116,6 +116,22 @@ function ChatMsgCtrl($scope, $routeParams, socket, lastTenMsg, topFiveMsg, histo
 			sendMessage();
 		}
 	});
+}
+
+
+function ChatHistoryCtrl($scope, $routeParams, historyMsg) {
+	$scope.messages = historyMsg.get({
+		room: $routeParams.room
+	});
+
+	$scope.back = function() {
+		window.history.back();
+	}
+}
+
+
+function NavbarCtrl($scope, $rootScope, $routeParams) {
+	$scope.room = $routeParams.room;
 
 	/*****************/
 	/* FB login code */
@@ -133,28 +149,16 @@ function ChatMsgCtrl($scope, $routeParams, socket, lastTenMsg, topFiveMsg, histo
 	function statusChangeCallback(response) {
 		console.log('statusChangeCallback');
 		console.log(response);
-		// The response object is returned with a status field that lets the
-		// app know the current login status of the person.
-		// Full docs on the response object can be found in the documentation
-		// for FB.getLoginStatus().
 		if (response.status === 'connected') {
 			// Logged into your app and Facebook.
 			successAPI();
 		} else if (response.status === 'not_authorized') {
-			// The person is logged into Facebook, but not your app.
-			// document.getElementById('status').innerHTML = 'Please log ' + 'into this app.';
 			failAPI();
 		} else {
-			// The person is not logged into Facebook, so we're not sure if
-			// they are logged into this app or not.
-			// document.getElementById('status').innerHTML = 'Please log ' + 'into Facebook.';
 			failAPI();
 		}
 	}
 
-	// This function is called when someone finishes with the Login
-	// Button.  See the onlogin handler attached to it in the sample
-	// code below.
 	function checkLoginState() {
 		FB.getLoginStatus(function(response) {
 			statusChangeCallback(response);
@@ -169,18 +173,6 @@ function ChatMsgCtrl($scope, $routeParams, socket, lastTenMsg, topFiveMsg, histo
 			xfbml: true, // parse social plugins on this page
 			version: 'v2.5' // use graph api version 2.5
 		});
-
-		// Now that we've initialized the JavaScript SDK, we call 
-		// FB.getLoginStatus().  This function gets the state of the
-		// person visiting this page and can return one of three states to
-		// the callback you provide.  They can be:
-		//
-		// 1. Logged into your app ('connected')
-		// 2. Logged into Facebook, but not your app ('not_authorized')
-		// 3. Not logged into Facebook and can't tell if they are logged into
-		//    your app or not.
-		//
-		// These three cases are handled in the callback function.
 
 		FB.getLoginStatus(function(response) {
 			statusChangeCallback(response);
@@ -208,29 +200,18 @@ function ChatMsgCtrl($scope, $routeParams, socket, lastTenMsg, topFiveMsg, histo
 				name: response.name,
 				id: response.id
 			});
-			$('.inputMessage').removeAttr('disabled');
-			$('.FBloginNote').text('Hello, ' + response.name);
-			//document.getElementById('status').innerHTML =
-			//  'Thanks for logging in, ' + response.id + '!';
+			// $('.inputMessage').removeAttr('disabled');
+			$rootScope.FBlogin = true;
+			// $('.FBloginNote').text('Hello, ' + response.name);
 		});
 	}
 
 	function failAPI() {
 		console.log("Facebook login fail.");
-		$('.inputMessage').attr('disabled', true);
-		$('.FBloginNote').text("Please login to chat");
-	}
-
-}
-
-
-function ChatHistoryCtrl($scope, $routeParams, historyMsg) {
-	$scope.messages = historyMsg.get({
-		room: $routeParams.room
-	});
-
-	$scope.back = function() {
-		window.history.back();
+		// $('.inputMessage')
+		//	.attr('placeholder', "Please login to chat");
+		$rootScope.FBlogin = false;
+		// $('.FBloginNote').text("Please login to chat");
 	}
 }
 
